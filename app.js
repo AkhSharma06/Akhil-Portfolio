86609c95d78484a8839f4d307ab08b4e553d4e47
Akhil <theakkuking@gmail.com>
2024-12-26 18:18:47 -0600

    Added app.js and added fade scrolling

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hiddenElements = document.querySelectorAll(".hidden");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    entry.target.classList.remove("hidden");
+                    // observer.unobserve(entry.target); // Stop observing once visible
+                } else {
+                    entry.target.classList.remove("visible");
+                }
+            });
+        },
+    );
+
+    // hiddenElements.forEach((el) => observer.observe(el));
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observer(el));
+
